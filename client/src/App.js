cfb5f399686cef75da04c955569471597eec085d
Bhavishya chauhan <bhavishya2468@gmail.com>
2025-11-05 09:54:29 +0530

    thank you route added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import Home from "./pages/Home";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import About from "./pages/About";
@@ -10,7 +10,12 @@ import OfcCable from "./pages/OfcCable";
 import ScrollToTop from "./ScrollToTop";
 import ThankYou from "./pages/ThankYou";
 
-const App = () => {
+const AppContent = () => {
+  const location = useLocation();
+
+  // List of routes where Navbar and Footer should be hidden
+  const hideLayout = ["/thank-you"];
+
   useEffect(() => {
     // --- Google Analytics (gtag.js) ---
     const gaScript = document.createElement("script");
@@ -52,36 +57,41 @@ const App = () => {
       </noscript>
       {/* End Google Tag Manager (noscript) */}
 
-      <BrowserRouter>
-        <ScrollToTop />
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/cfdsi/about-us/underground-cable-fault-repair-experts"
-            element={<About />}
-          />
-          <Route
-            path="/cfdsi/ht-cable-repair-services/high-voltage-cable-fault-detection-repair"
-            element={<HtCable />}
-          />
-          <Route
-            path="/cfdsi/lt-cable-repair-services/low-voltage-cable-fault-detection-repair"
-            element={<LtCabel />}
-          />
-          <Route
-            path="/cfdsi/ofc-cable-repair-services/optical-fiber-cable-fault-repair-detection"
-            element={<OfcCable />}
-          />
-          <Route
-            path="/thank-you"
-            element={<ThankYou/>}
-          />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <ScrollToTop />
+
+      {/* Conditionally render Navbar/Footer */}
+      {!hideLayout.includes(location.pathname) && <Navbar />}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/cfdsi/about-us/underground-cable-fault-repair-experts"
+          element={<About />}
+        />
+        <Route
+          path="/cfdsi/ht-cable-repair-services/high-voltage-cable-fault-detection-repair"
+          element={<HtCable />}
+        />
+        <Route
+          path="/cfdsi/lt-cable-repair-services/low-voltage-cable-fault-detection-repair"
+          element={<LtCabel />}
+        />
+        <Route
+          path="/cfdsi/ofc-cable-repair-services/optical-fiber-cable-fault-repair-detection"
+          element={<OfcCable />}
+        />
+        <Route path="/thank-you" element={<ThankYou />} />
+      </Routes>
+
+      {!hideLayout.includes(location.pathname) && <Footer />}
     </>
   );
 };
+
+const App = () => (
+  <BrowserRouter>
+    <AppContent />
+  </BrowserRouter>
+);
 
 export default App;
